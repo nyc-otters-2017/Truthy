b3d99a999e676503cda44c9863a22b94db72a91c
Jefferson-Faseler <jjfaseler@gmail.com>
2017-02-03 18:14:40 -0500

    Restart skeleton for analysis model.

--- app/models/analysis.rb
+++ app/models/analysis.rb
@@ -1,16 +1,14 @@
 class Analysis < ApplicationRecord
 
+  WATSON_DATA = "https://gateway-a.watsonplatform.net/calls/url/URLGetCombinedData?url="
+  WATSON_TEXT = "https://gateway-a.watsonplatform.net/calls/url/URLGetText?url="
+  WATSON_INFO = "/&outputMode=json&extract=title,author=1&maxRetrieve=3&"
+  WATSON_CLOSING = "&outputMode=json&extract&apikey="
+
   def self.watson_call(params)
-  service = WatsonAPIClient::AlchemyLanguage.new(:apikey=>ENV['WATSON_KEY'],
-                                                 :verify_ssl=>OpenSSL::SSL::VERIFY_NONE)
-  @watson_call = service.URLGetCombinedData('model'      => 'en-news',
-                                      'extract'    => 'authors, doc-emotion, entities, keywords, doc-sentiment, taxonomy, title',
-                                      'url'        => params['url'],
-                                      'outputMode' => 'json')
-  debugger
+    @response = {}
+    @response[:data] = WATSON_DATA << params['url'] << WATSON_CLOSING << ENV['WATSON_KEY']
+    
   end
-
-
-  response = service.URLGetTypedRelations('model'      => 'en-news','url'        => 'www.cnn.com', 'outputMode' => 'json')
 
 end
