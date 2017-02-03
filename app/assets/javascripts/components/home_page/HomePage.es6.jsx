6af3ed9653489b5fb22bee9469cb51f7c765a171
[01;31m[K"name"[m[K: "kevin hu <[01;31m[K"email"[m[K: null>
2017-02-03 15:26:01 -0500

    Helper for checking URL exsit

--- app/assets/javascripts/components/home_page/HomePage.es6.jsx
+++ app/assets/javascripts/components/home_page/HomePage.es6.jsx
@@ -12,6 +12,7 @@ class HomePage extends React.Component {
       data: {urlString: this.refs.urlString.value}
     })
      .done(response=>{
+       debugger
        console.log('sending...', this.refs.urlString.value)
        console.log('success handler', response)
      })
