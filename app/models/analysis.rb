36dd1340796b8a9d88ab6d8beffd9a99a65756a5
Jefferson-Faseler <jjfaseler@gmail.com>
2017-02-04 13:48:48 -0500

    Adjust logic for author and finish response gathering.

--- app/models/analysis.rb
+++ app/models/analysis.rb
@@ -6,7 +6,7 @@ class Analysis < ApplicationRecord
  WATSON_CLOSING = "&outputMode=json&extract&apikey="
 
  def self.watson_call(urlString)
-   @opened_uri = {}
+   @opened_uri, @response = {}, {}
    @data = WATSON_DATA + urlString + WATSON_CLOSING + ENV['WATSON_KEY']
    @text = WATSON_TEXT + urlString + WATSON_CLOSING + ENV['WATSON_KEY']
    @info = WATSON_DATA + urlString + WATSON_INFO + WATSON_CLOSING + ENV['WATSON_KEY']
@@ -14,33 +14,35 @@ class Analysis < ApplicationRecord
    @opened_uri[:text] = JSON.parse(open(@text, 'Accept-Encoding' => '') {|f| f.read })
    @opened_uri[:info] = JSON.parse(open(@info, 'Accept-Encoding' => '') {|f| f.read })
    stage_render
+   @response
  end
 
  private
 
  def self.stage_render
-   binding.pry
+   @response['text'] = @opened_uri[:text]['text']
+   @response['title'] = @opened_uri[:info]['title']
+   @response['author'] = @opened_uri[:info]['author'] || 'No author is listed'
    @response['taxonomy'] = @opened_uri[:data]['taxonomy']
    @response['concepts'] = keep_relevant_concepts(@opened_uri[:data]['concepts'])
    @response['entities'] = keep_relevant_entities(@opened_uri[:data]['entities'])
    @response['keywords'] = keep_relevant_keywords(@opened_uri[:data]['keywords'])
-
  end
 
- def keep_relevant_concepts(result)
-   result.keep_if |r| do
+ def self.keep_relevant_concepts(result)
+   result.keep_if do |r|
      r['relevance'].to_f >= 0.8
    end
  end
 
- def keep_relevant_entities(result)
-   result.keep_if |r| do
+ def self.keep_relevant_entities(result)
+   result.keep_if do |r|
      r['relevance'].to_f >= 0.7
    end
  end
 
- def keep_relevant_keywords(result)
-   result.keep_if |r| do
+ def self.keep_relevant_keywords(result)
+   result.keep_if do |r|
      r['relevance'].to_f >= 0.6
    end
  end
