84f6b755b41feaa07598c5ce0a2294fa0ff64489
John Talio <johntalio@yahoo.com>
2017-02-04 16:38:01 -0500

    Remove button from parent component and created child component for it

--- app/assets/javascripts/components/result_page/ResultPage.es6.jsx
+++ app/assets/javascripts/components/result_page/ResultPage.es6.jsx
@@ -1,25 +1,15 @@
 class ResultPage extends React.Component {
   constructor() {
     super()
-    this.handleClick = this.handleClick.bind(this)
   }
 
-  handleClick(event) {
-    event.preventDefault()
-    console.log("yay")
-    $.ajax({
-      url: '/results'
-    })
-    .done(response => {
-      console.log("response", response)
-    })
-  }
+
 
   render() {
     return(
       <div className='result-container'>
-        <input type="button" name="revealResults" onClick={this.handleClick} value="Article Status" />
         <BlackList fullContact={this.props.fullContact} />
+        <ArticleStatus blackList={this.props.blackList} />
       </div>
     )
   }
