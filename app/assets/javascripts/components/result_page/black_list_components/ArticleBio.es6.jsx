33326a99e30f00abd789af333c891486b46e90b5
John Talio <johntalio@yahoo.com>
2017-02-04 19:13:32 -0500

    MouseLeave working + very small adjustment on blacklist (#31)

--- app/assets/javascripts/components/result_page/black_list_components/ArticleBio.es6.jsx
+++ app/assets/javascripts/components/result_page/black_list_components/ArticleBio.es6.jsx
@@ -2,18 +2,22 @@ class ArticleBio extends React.Component {
   constructor() {
     super()
     this.handleMouseOver = this.handleMouseOver.bind(this)
+    this.handleMouseLeave = this.handleMouseLeave.bind(this)
     this.state = { articleBio: "" }
   }
 
   handleMouseOver() {
-    this.setState({articleBio: this.props.bio })
+    this.setState({articleBio: this.props.bio})
   }
 
+  handleMouseLeave() {
+    this.setState({articleBio: ""})
+  }
 
   render() {
     return(
       <section id="article-bio">
-        <h1 id="article-header" onMouseOver={this.handleMouseOver}>Article Bio</h1>
+        <h1 id="article-header" onMouseOver={this.handleMouseOver} onMouseLeave={this.handleMouseLeave}>Article Bio</h1>
         <p>{this.state.articleBio}</p>
       </section>
     )
