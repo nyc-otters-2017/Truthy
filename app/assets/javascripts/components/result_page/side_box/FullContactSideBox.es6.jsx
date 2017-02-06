4170837a99c5a49ec926bffbf6f8a4fe13e8649c
Jefferson-Faseler <jjfaseler@gmail.com>
2017-02-06 10:01:07 -0500

    Implement some changes.

--- app/assets/javascripts/components/result_page/side_box/FullContactSideBox.es6.jsx
+++ app/assets/javascripts/components/result_page/side_box/FullContactSideBox.es6.jsx
@@ -12,11 +12,21 @@ class FullContactSideBox extends React.Component {
 
 
   render() {
+    const transitionOptions = {
+      transitionEnterTimeout: 500,
+      transitionLeaveTimeout: 500
+    }
     return (
-      <div>
-        <h4 className="side_box_title text-center">Get company bio</h4>
-        <p>something something about this box</p>
-        <p className="text-right"><a onClick={this.handleClick} className="btn btn-primary btn-sm" href="#" role="button">Learn more</a></p>
+      <div className="hovereffect">
+
+        <div className="overlay">
+          <h4 className="side_box_title text-center">Get Company Bio</h4>
+          <p>something something about this box</p>
+          <p className="text-right"><a onClick={this.handleClick} className="btn btn-primary btn-sm" href="#" role="button">Learn more</a></p>
+        </div>
+
+        <i className="img-responsive material-icons orange600">donut_large</i>
+
       </div>
     )
   }
--- app/assets/javascripts/components/result_page/side_box/SideBox.es6.jsx
+++ app/assets/javascripts/components/result_page/side_box/SideBox.es6.jsx
@@ -3,15 +3,15 @@ class SideBox extends React.Component {
   render() {
     return (
       <div>
-        <div className="">
+        <div className="side_box">
           <FullContactSideBox onNewRender={this.props.onNewRender} data={this.props.fullContact}/>
         </div>
 
-        <div className="">
+        <div className="side_box">
           <WatsonBodySideBox onNewRender={this.props.onNewRender} data={this.props.watson}/>
         </div>
 
-        <div className="">
+        <div className="side_box">
           <WatsonEverythingElseSideBox onNewRender={this.props.onNewRender} data={this.props.watson}/>
         </div>
       </div>
