bfc03aae4680ba1bef095321ae753aab2f1830bd
Jefferson Faseler <jjfaseler@gmail.com>
2017-02-06 13:42:50 -0500

    Aylien component (#40)
    * Add working Aylien methods. Sending as props.
    
    * Removed debugger.

--- app/helpers/aylien_helper.rb
+++ app/helpers/aylien_helper.rb
@@ -1,5 +1,11 @@
 module AylienHelper
-  def self.aylien_call(title='donald trump', entities=[])
+  def stage_aylien(data)
+    @keyword = data['keywords'].first['text']
+    entities = data['concepts'].select {|h| h.has_key?('dbpedia')}
+    @entities = data['concepts'].map {|h| h['dbpedia']}
+  end
+
+  def self.aylien_call(keyword, entities)
     AylienNewsApi.configure do |config|
       # Configure API key authorization: app_id
       config.api_key['X-AYLIEN-NewsAPI-Application-ID'] = ENV['AYLIEN_APP']
@@ -11,7 +17,7 @@ module AylienHelper
     api_instance = AylienNewsApi::DefaultApi.new
 
     opts = {
-      :title => title,
+      :title => keyword,
       :published_at_start => "NOW-7DAYS",
       :published_at_end => "NOW",
       :entities_body_links_dbpedia => entities,
