e99d81de15ee20711184d0620181e8ac946c987d
John Talio <johntalio@yahoo.com>
2017-02-06 16:07:13 -0500

    DocumentEmotion component rendering its data

---
+++ app/assets/javascripts/components/result_page/Watson/DocumentEmotion.es6.jsx
@@ -0,0 +1,19 @@
+class DocumentEmotion extends React.Component {
+  render() {
+    // debugger
+    return(
+      <div>
+        <h3>Document Emotions</h3>
+        <ul>
+          {
+            this.props.docEmotions.map((emotion, i) => {
+              return(
+                <li key={i}>{emotion.emotion}, {emotion.score*100 + "%"}</li>
+              )
+            })
+          }
+        </ul>
+      </div>
+    )
+  }
+}
