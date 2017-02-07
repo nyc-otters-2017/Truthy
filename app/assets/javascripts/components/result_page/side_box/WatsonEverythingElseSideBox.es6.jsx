adef9319404caa6c02211b1b8823fe2f86081036
[01;31m[K"name"[m[K: "kevin hu <[01;31m[K"email"[m[K: null>
2017-02-07 16:16:05 -0500

    Side bar stuff

--- app/assets/javascripts/components/result_page/side_box/WatsonEverythingElseSideBox.es6.jsx
+++ app/assets/javascripts/components/result_page/side_box/WatsonEverythingElseSideBox.es6.jsx
@@ -14,11 +14,11 @@ class WatsonEverythingElseSideBox extends React.Component {
       <div className="hovereffect2 sidebar1">
 
         <div className="overlay">
-          <h4 className="side_box_title text-center">Article Data</h4>
+          <h4 className="side_box_title">Article Data</h4>
           <p className="about_company">View the sentiment, subjectivity, emotions, and other important data about the article.</p>
-          <p className="text-right padding botton_corner"><a onClick={this.handleClick} className="btn btn-primary btn-sm" href="#" role="button">View Data</a></p>
+          <p className="side_bar_view">
+            <a onClick={this.handleClick} className="btn btn-primary btn-sm" href="#" role="button">View</a></p>
         </div>
-
         <i className="img-responsive material-icons orange600">donut_large</i>
 
       </div>
