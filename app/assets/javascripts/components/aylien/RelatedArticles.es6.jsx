01f4c6c95c969608222f095de326a4c34789847a
Jefferson Faseler <jjfaseler@gmail.com>
2017-02-07 11:39:14 -0500

    Aylien styling (#51)
    * Fix errors. Prepare for transitions.
    
    * Change environment to allow for react add ons. Style aylien component.

--- app/assets/javascripts/components/aylien/RelatedArticles.es6.jsx
+++ app/assets/javascripts/components/aylien/RelatedArticles.es6.jsx
@@ -1,24 +1,54 @@
 class RelatedArticles extends React.Component {
-    constructor() {
-        super()
-    }
 
-    render() {
-      console.log(this.props.articles)
-        return (
-          <div class="container">
-            {this.props.articles.map((article, i) => {
-              return (
-                <a href={article.link}>
-                  <div className='related-article' key={i}>
+  constructor() {
+    super()
+    this.state = {
+      revealed: false,
+    }
+    this.handleClick = this.handleClick.bind(this)
+  }
+
+  handleClick(event) {
+    event.preventDefault()
+    let status = this.state.revealed
+    this.setState({revealed: !status})
+  }
+
+
+  render(){
+    const yourArticlesAreShowing = this.state.revealed
+    var ReactCSSTransitionGroup = React.addons.CSSTransitionGroup
+
+    if (yourArticlesAreShowing) {
+      var articles = (
+          this.props.articles.map((article, i)=>{
+          return (
+            <ReactCSSTransitionGroup
+              transitionName="related"
+              transitionEnterTimeout={1000}
+              transitionLeaveTimeout={600}
+              key={i}>
+              <div className='related-article' >
+                <div className='related-article-info'>
+                  <a href={article.link} >
                     <img className='related-article' src={article.media}/>
                     <br/>
                     <span>{article.title}</span>
-                  </div>
-                </a>
-              )
-            })}
-          </div>
-        )
+                  </a>
+                </div>
+              </div>
+            </ReactCSSTransitionGroup>
+          )
+        })
+      )
     }
+
+
+    return(
+      <div>
+        <a href="#" onClick={this.handleClick}><h1>Related Articles</h1></a>
+          { articles }
+      </div>
+    )
+  }
 }
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -18,6 +18,9 @@ Rails.application.configure do
   # Apache or NGINX already handles this.
   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
 
+  # Adds React-add-ons package version of React
+  config.react.addons = true # defaults to false
+
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
