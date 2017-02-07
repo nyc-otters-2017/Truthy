cb8c3d76aba0ee34f634a52d51950c3853ae2efd
[01;31m[K"name"[m[K: "kevin hu <[01;31m[K"email"[m[K: null>
2017-02-07 15:42:40 -0500

    Fix buttons

--- app/assets/javascripts/components/result_page/side_box/QuotesSideBox.es6.jsx
+++ app/assets/javascripts/components/result_page/side_box/QuotesSideBox.es6.jsx
@@ -16,7 +16,7 @@ class QuotesSideBox extends React.Component {
         <div className="hovereffect2 sidebar1">
           <div className="overlay">
             <h4 className="side_box_title text-center">See Quotes</h4>
-            <p>View all the extracted quotes from article for verification</p>
+            <p className="about_company">View all the extracted quotes from article for verification</p>
             <p className="text-right padding botton_corner"><a onClick={this.handleClick} className="btn btn-primary btn-sm" href="#" role="button">View Quotes</a></p>
           </div>
           <i className="img-responsive material-icons orange600">language</i>
--- app/assets/javascripts/components/result_page/side_box/WatsonEverythingElseSideBox.es6.jsx
+++ app/assets/javascripts/components/result_page/side_box/WatsonEverythingElseSideBox.es6.jsx
@@ -15,7 +15,7 @@ class WatsonEverythingElseSideBox extends React.Component {
 
         <div className="overlay">
           <h4 className="side_box_title text-center">Article Data</h4>
-          <p className="box_discriptions">View the sentiment, subjectivity, emotions, and other important data about the article.</p>
+          <p className="about_company">View the sentiment, subjectivity, emotions, and other important data about the article.</p>
           <p className="text-right padding botton_corner"><a onClick={this.handleClick} className="btn btn-primary btn-sm" href="#" role="button">View Data</a></p>
         </div>
 
