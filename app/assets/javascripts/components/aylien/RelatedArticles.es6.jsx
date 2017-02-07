b98b89fd788a6e9981b3e5291e3eedd9725c62f7
Jefferson-Faseler <jjfaseler@gmail.com>
2017-02-07 10:25:21 -0500

    Fix errors. Prepare for transitions.

--- app/assets/javascripts/components/aylien/RelatedArticles.es6.jsx
+++ app/assets/javascripts/components/aylien/RelatedArticles.es6.jsx
@@ -1,24 +1,51 @@
 class RelatedArticles extends React.Component {
-    constructor() {
-        super()
+
+  constructor() {
+    super()
+    this.state = {
+      revealed: false,
+    }
+    this.handleClick = this.handleClick.bind(this)
+  }
+
+  handleClick(event) {
+    event.preventDefault()
+    let status = this.state.revealed
+    this.setState({revealed: !status})
+  }
+
+
+  render(){
+    const yourArticlesAreShowing = this.state.revealed
+    var ReactCSSTransitionGroup = React.addons.CSSTransitionGroup
+
+    if (yourArticlesAreShowing) {
+      var articles = (
+          this.props.articles.map((article, i)=>{
+          return (
+            <a href={article.link} key={i}>
+              <div className='related-article' >
+              <img className='related-article' src={article.media}/>
+              <br/>
+              <span>{article.title}</span>
+            </div>
+            </a>
+          )
+        })
+      )
     }
 
-    render() {
-      console.log(this.props.articles)
-        return (
-          <div class="container">
-            {this.props.articles.map((article, i) => {
-              return (
-                <a href={article.link}>
-                  <div className='related-article' key={i}>
-                    <img className='related-article' src={article.media}/>
-                    <br/>
-                    <span>{article.title}</span>
-                  </div>
-                </a>
-              )
-            })}
-          </div>
-        )
-    }
+
+    return(
+      <div>
+        <a href="#" onClick={this.handleClick}>Related articles</a>
+        <ReactCSSTransitionGroup
+          transitionName="related"
+          transitionEnterTimeout={500}
+          transitionLeaveTimeout={300}>
+          { articles }
+        </ReactCSSTransitionGroup>
+      </div>
+    )
+  }
 }
