ff8d2a856530dd3d94768bfb6cd971d0c6d47c5e
[01;31m[K"name"[m[K: "kevin hu <[01;31m[K"email"[m[K: null>
2017-02-07 00:37:35 -0500

    Add scroll bar for video side bar

--- app/assets/javascripts/components/aylien/RelatedArticles.es6.jsx
+++ app/assets/javascripts/components/aylien/RelatedArticles.es6.jsx
@@ -1,24 +1,24 @@
 class RelatedArticles extends React.Component {
-  constructor() {
-    super()
-  }
+    constructor() {
+        super()
+    }
 
-  render(){
-    console.log(this.props.articles)
-    return(
-      <div>
-      {this.props.articles.map((article, i)=>{
+    render() {
+      console.log(this.props.articles)
         return (
-          <a href={article.link}>
-          <div className='related-article' key={i}>
-            <img className='related-article' src={article.media}/>
-            <br/>
-            <span>{article.title}</span>
+          <div class="container">
+            {this.props.articles.map((article, i) => {
+              return (
+                <a href={article.link}>
+                  <div className='related-article' key={i}>
+                    <img className='related-article' src={article.media}/>
+                    <br/>
+                    <span>{article.title}</span>
+                  </div>
+                </a>
+              )
+            })}
           </div>
-          </a>
         )
-      })}
-    </div>
-    )
-  }
+    }
 }
--- app/assets/javascripts/components/result_page/article_info_components/ArticleInfo.es6.jsx
+++ app/assets/javascripts/components/result_page/article_info_components/ArticleInfo.es6.jsx
@@ -3,7 +3,7 @@ class ArticleInfo extends React.Component {
   render() {
     return (
       <div>
-        <ArticleBio bio={this.props.data.bio} />
+        {/* <ArticleBio bio={this.props.data.bio} /> */}
         <SocialMedia data={this.props.data} />
       </div>
     )
