6d6a78ce3a8bd380f505973ef5a82fd5271d4dc3
Jefferson-Faseler <jjfaseler@gmail.com>
2017-02-08 13:24:02 -0500

    Update logic to turn month into abbreviated name.

--- app/assets/javascripts/components/result_page/Clock.es6.jsx
+++ app/assets/javascripts/components/result_page/Clock.es6.jsx
@@ -13,6 +13,35 @@ class Clock extends React.Component {
 	}
 
 
+	monthAsWord(number) {
+		switch (number) {
+			case 0:
+				return 'Jan'
+			case 1:
+				return 'Feb'
+			case 2:
+				return 'Mar'
+			case 3:
+				return 'Apr'
+			case 4:
+				return 'May'
+			case 5:
+				return 'June'
+			case 6:
+				return 'July'
+			case 7:
+				return 'Aug'
+			case 8:
+				return 'Sept'
+			case 9:
+				return 'Oct'
+			case 10:
+				return 'Nov'
+			case 11:
+				return 'Dec'
+		}
+	}
+
 	componentDidMount() {
 		setInterval(() => {
 			var p = new Date();
@@ -20,7 +49,7 @@ class Clock extends React.Component {
 			m: p.getMinutes(),
 			s: p.getSeconds(),
 			h: p.getHours(),
-			month: p.getMonth() + 1,
+			month: this.monthAsWord(p.getMonth()),
 			day: p.getDate(),
 			year: p.getFullYear()
 			})
