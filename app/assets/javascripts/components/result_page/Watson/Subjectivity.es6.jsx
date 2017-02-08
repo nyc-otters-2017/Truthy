a71aa4cbf62b1278c70905743f02f84934fee8dc
Jefferson Faseler <jjfaseler@gmail.com>
2017-02-08 17:46:58 -0500

    Data style update (#77)
    * Update styling of data page.
    
    * Change layout and styling of data page.:
    
    * Change styling for concepts and data layout.

--- app/assets/javascripts/components/result_page/Watson/Subjectivity.es6.jsx
+++ app/assets/javascripts/components/result_page/Watson/Subjectivity.es6.jsx
@@ -1,12 +1,10 @@
 class Subjectivity extends React.Component {
   render() {
     return(
-      <div className="Subjectivity-container">
-        <h2>Subjectivity Analysis</h2>
-        <h4>Determines whether the content is primarily objective or subjective.</h4>
-        <ul>
-          {this.props.subjectivity}
-        </ul>
+      <div className="data-container">
+        <h3>Subjectivity Analysis</h3>
+        <h1>{this.props.subjectivity.charAt(0).toUpperCase() + this.props.subjectivity.slice(1)}</h1>
+        <h4>Determines whether the content is primarily opinion or fact-driven.</h4>
       </div>
     )
   }
