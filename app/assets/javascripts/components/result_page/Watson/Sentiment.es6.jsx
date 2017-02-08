f68ca19c86fa1309bb5a534a7bfbb4f26b95ce85
Jefferson-Faseler <jjfaseler@gmail.com>
2017-02-08 16:25:52 -0500

    Update styling of data page.

--- app/assets/javascripts/components/result_page/Watson/Sentiment.es6.jsx
+++ app/assets/javascripts/components/result_page/Watson/Sentiment.es6.jsx
@@ -1,12 +1,10 @@
 class Sentiment extends React.Component {
   render() {
     return(
-      <div className="sentiment-container">
-        <h2>Sentiment</h2>
+      <div className="data-container">
+        <h3>Sentiment</h3>
+        <h1>{this.props.sentiment.charAt(0).toUpperCase() + this.props.sentiment.slice(1)}</h1>
         <h4>Assesses the whether the sentiment of the content is positive, negative, or neutral.</h4>
-        <ul>
-          {this.props.sentiment}
-        </ul>
       </div>
     )
   }
