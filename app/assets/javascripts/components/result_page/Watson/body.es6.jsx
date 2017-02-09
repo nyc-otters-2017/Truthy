c436993688109db37c965a22cda273f1adec9b51
John Talio <johntalio@yahoo.com>
2017-02-09 14:46:48 -0500

    Publication date fix (#95)
    * Publication date displaying on site - need to format
    
    * Can display formatted pub date - pass props through result page
    
    * Fix props for pub date - working
    
    * Create instance variable for pub date and pass it to result view for component

--- app/assets/javascripts/components/result_page/Watson/body.es6.jsx
+++ app/assets/javascripts/components/result_page/Watson/body.es6.jsx
@@ -4,6 +4,7 @@ class Body extends React.Component {
     this.state = {display: true}
   }
   render() {
+    debugger
 
     return (
       <div className="scroll-change">
@@ -11,10 +12,9 @@ class Body extends React.Component {
 
         <div className="newspaper_bottom_small_line top_padding">
         </div>
-        <p id="author_stuff" className="text-center">By {this.props.data.author} | Written on {this.props.data.publicationDate}</p>
+        <p id="author_stuff" className="text-center">By {this.props.data.author} | Written on {this.props.publicationDate}</p>
         <div className="newspaper_bottom_small_line">
         </div>
-        {/* <p>Written on {this.props.data.publicationDate}</p> */}
         <p id="text_indent" className="top_padding" >{this.props.data.text}</p>
 
       </div>
