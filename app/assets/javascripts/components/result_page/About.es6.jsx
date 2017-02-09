a58e8c1c3212419a14e2c6bae18ce4a00c039712
John Talio <johntalio@yahoo.com>
2017-02-09 12:00:05 -0500

    Changes to about info - grammar and other fixes

--- app/assets/javascripts/components/result_page/About.es6.jsx
+++ app/assets/javascripts/components/result_page/About.es6.jsx
@@ -6,22 +6,25 @@ class About extends React.Component {
         <h1>About Truthy</h1>
         <p>First, thank you for using this site to learn more about your news sources,
           while also teaching our algorithm about reliable news sources!</p>
-        <p>The way Truthy works is by collecting data about the url for a news source
-        you put into the search bar. Our server will collect data from the internet, and
-        give you valuable information to determine whether or not the article is fake news(Truthy or Falsey).
+        <p>Truthy works by collecting data about a news article from the url
+        you enter into the search bar. Our server will aggregate data from the internet and
+        present you with valuable information you can use to determine whether or not the article is
+        fake news (Truthy or Falsey).
         </p>
         <p>
           Our custom machine learning model relies on people like you to read the article,
-          assess the company and author who wrote it, the data about the article structure,
-          and ensure that the quotes extracted from the article are accurate.
+          assess its source (the author who wrote it and the company that published it),
+          review the data we've gathered about the article, and ensure that the quotes extracted from
+          the article provide accurate and reliable information.
+        </p>
+        <h1>Navigating Truthy</h1>
+        <p>
+          On the left you will see articles that our algorithm believes to be similar to the piece
+          you are reading, which is based on keywords and concepts. This is important because fake news
+          is often completely unique in its content. Just be aware that there are exceptions to this rule.
         </p>
         <p>
-          On the left you will see articles based on keywords and concepts that our algorithm
-          believes to be similar to the piece you are reading. This is important because fake news
-          is often completely unique in its content. Just be aware that there are exceptions to this rule of thumb.
-        </p>
-        <p>
-          On your right you will see little spinning buttons that will guide you to the
+          On the right you will see little spinning buttons that will guide you to
           information about article data, company info, and extracted quotes.
         </p>
         <p>
